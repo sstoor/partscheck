1e8c27e7abf13fbb43a5e963ab079bf2ce66c3b5
Sukh Singh <54824217+sstoor@users.noreply.github.com>
2022-08-30 14:01:48 +1000

    html and structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { QueryClientProvider, QueryClient } from "react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>PartsCheck OEM Suppliers</h1>
+        <div className="carpartlist">
+          <div className="left">
+            <div className="Suppliername">Sup name</div>
+            <div className="stars">*****</div>
+            <div className="vehiclemake">make, make, make</div>
+          </div>
+          <div className="right">supplier state</div>
+        </div>
+      </div>
+    </QueryClientProvider>
   );
 }
 
